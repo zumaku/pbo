93e009018748a3543eb4e9bb969f6a41a9334585
Zuma <116003626+zumaku@users.noreply.github.com>
2022-11-08 22:04:56 +0800

    Add files via upload

---
+++ pbo4/laporan.py
@@ -0,0 +1,35 @@
+# soal 1 dan 2
+# class mahasiswa:
+#     kampus = ''
+#     jurusan = ''
+#     angakatan = ''
+
+#     def __init__(self):
+#         self.kampus = 'UIN Alauddin Makassar'
+#         self.jurusan = 'Teknik Informatika'
+#         self.angakatan = '2021'
+
+# mahasiswa = mahasiswa()
+# print (mahasiswa.kampus)
+# print (mahasiswa.jurusan)
+# print (mahasiswa.angakatan)
+
+# soal 3
+class keluarga:
+    jumlahAnak = ''
+    namaAyah = ''
+    namaIbu = ''
+    alamat = ''
+    pekerjaanAyah = ''
+
+    def __init__(self):
+        self.jumlahAnak = 0
+    
+    def nmAy(self):
+        self.namaAyah = 'Nur Ahmad'
+
+    def nmIb(self):
+        self.namaIbu = 'Andi Kebo'
+
+    def pkAy(self):
+        self.pekerjaanAyah = 'Petani'
